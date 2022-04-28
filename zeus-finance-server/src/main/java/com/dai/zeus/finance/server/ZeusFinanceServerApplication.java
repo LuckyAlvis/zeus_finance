36d2622bd4548098bb42f35d7127d7e518c55349
dyf_Home <252127196@qq.com>
2022-04-29 05:40:14 +0800

    初始化项目

---
+++ zeus-finance-server/src/main/java/com/dai/zeus/finance/server/ZeusFinanceServerApplication.java
@@ -0,0 +1,13 @@
+package com.dai.zeus.finance.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusFinanceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusFinanceServerApplication.class, args);
+    }
+
+}
---
+++ zeus-finance-server/src/test/java/com/dai/zeus/finance/server/ZeusFinanceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dai.zeus.finance.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusFinanceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
